6638ae8b919c8c59656935e4f44a93222aff5c8f
Matthew Day <matt@matthewday.net>
2020-09-04 10:42:15 -0400

    Add API call for weather data

--- index.js
+++ index.js
@@ -12,3 +12,16 @@ function loadnews () {
         console.log(news);
     })
 }
+
+// TODO detect location based on the user's IP address
+const someZipCode = "90210";
+// TODO sign up for an account at openweathermap in order to get an API key
+const someAPIKey = apiKey;
+
+function getWeatherData() {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${someZipCode},us&appid=${someAPIKey}`)
+  .then(data => data.json())
+  .then(jsonData => console.log(`\nWEATHER DATA\n${JSON.stringify(jsonData, null, 2)}\n`));
+}
+
+getWeatherData();
---
+++ preview.js
@@ -0,0 +1,1 @@
+const apiKey = "5006b1aca9833e7dc94ec977afb38019";
