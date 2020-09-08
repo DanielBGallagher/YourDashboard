0e59edc320942503f2994babe1d3b4128642d222
Daniel Gallagher <danielbgallagher@gmail.com>
2020-09-08 19:35:58 -0400

    article imgs now appear

--- index.js
+++ index.js
@@ -7,17 +7,21 @@ $().ready(() =>{
 })
 
 function loadNews () {
-    fetch('https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=COws2HNoQFFRrMk8kc77p3JoINr90whz')
+    fetch(`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${nyTimes}`)
     .then(news => news.json())
     .then(data => {
         
         let articleArray = data.results;
 
         const articleHTML = articleArray.map(currentArticle =>{
-            console.log(currentArticle.media);
+            
+            console.log(currentArticle.media[0]);
+
+            const imgSRC = currentArticle.media[0] ? currentArticle.media[0]["media-metadata"][0].url : "";
+
             $(".content").append(`<div class = "news-card2">
             <div class = "story-img2">
-              <img src = "">
+              <img src = "${imgSRC}">
               <p>${currentArticle.published_date}</p>
             </div>
             <div class = "story-info">
@@ -34,11 +38,10 @@ function loadNews () {
 
 // TODO detect location based on the user's IP address
 const someZipCode = "90210";
-// TODO sign up for an account at openweathermap in order to get an API key
-const someAPIKey = apiKey;
+
 
 function getWeatherData() {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${someZipCode},us&appid=${someAPIKey}`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${someZipCode},us&appid=${weather}`)
   .then(data => data.json())
   .then(jsonData => console.log(`\nWEATHER DATA\n${JSON.stringify(jsonData, null, 2)}\n`));
 }
