9f26bc5004c2c61b673394fd80f30273a66caa21
Christy <appletoms99@outlook.com>
2020-09-21 12:57:20 -0500

    update zip code search

--- weather.js
+++ weather.js
@@ -1,22 +1,47 @@
-$().ready(() =>{
+
+
+// var fetchWeather;
+// var api = "http://api.openweathermap.org/data/2.5/weather?zip=";
+// var apikey = "8c5785d2d6e48e5c2e504db748496ff8";
+// var input;
+
+
+// function getZip () {
+//   createCanvas(400, 200);
+//   var button = select('.zip');
+//   button.mousePressed(askWeather);
+
+//   input = select('.zip');
+// }
+
+// function askWeather () {
+//   var url = api + zip + input.value() + apikey;
+//   loadJSON(url, gotData);
+// }
+
+
+// function gotData (data) {
+//   fetchWeather = data;
+// }
+
+// $().ready(() =>{
 
 
 
-  getWeatherData();
+//   getWeatherData();
 
 
-})
+// })
 
-//getElementById('search').addEventListener()
+const someZipCode = document.getElementById('zip').value;
 
-//$('.form').click()
-
-function getWeatherData(zip) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${someZipCode},us&appid=${weatherapi}`)
+function getWeatherData() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${someZipCode}&appid=${weather}`)
     .then(data => data.json())
     .then(jsonData => {
       
-    let zip =""
+
+
 
     console.log(`\nWEATHER DATA\n${JSON.stringify(jsonData, null, 2)}\n`);
 
@@ -40,76 +65,44 @@ function getWeatherData(zip) {
       <h5>${fahrenheitMin.toFixed()} &#176; Fahrenheit</h5>
       <h5>${fahrenheitMax.toFixed()} &#176; Fahrenheit</h5>
       <h5>${jsonData.weather[0].description} </h5>`)
+      
 
-
-      // const form = document.getElementById( "searchbar" );
-
-      // form.addEventListener( "button", function (event) {
-      //   event.preventDefault();
-
-
-      // })
- 
     }
     
 
-    )}
 
-
-// function getWeatherData() {
-//   fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${someZipCode},us&appid=${weather}`)
-//   .then(data => data.json())
-//   .then(jsonData => console.log(`\nWEATHER DATA\n${JSON.stringify(jsonData, null, 2)}\n`));
-
-// }
-
-// getWeatherData();
-
+    )
+  
+  }
 
 
 // TODO detect location based on the user's Zip Code (IP address)
-// function getZip (someZipCode) {
 
-//   const someZipCode = "";
 
-//   for(i=0; i<zip; i++) {}
+// function getZip () {
+
+//   var request;
+//   var input1 = document.getElementById('zip');
+//   var apiCall = 'https://api.openweathermap.org/data/2.5/weather?zip=${input1},us&appid=${weather}'
+
+//   var weatherbyzip = apiCall + input1.value;
+
+//   request = new XMLHttpRequest();
+
+//   request.open('GET', weatherbyzip, true);
+//   request.onload = function() {
+//     var data = JSON.parse(this.response);
+//     if (request.status >=200 && request.status < 400) {
+//       console.log(data);
+//     }
+
+//     else {
+//       console.log(input1.value);
+//     }
+//   }
+
+//   request.send();
 
 // }
 
-const someZipCode = "90210";
 
-function getZip () {
-
-  var request;
-  var input1 = document.getElementById('searchbyzip');
-  var apiCall = 'https://api.openweathermap.org/data/2.5/weather?zip=${input1},us&appid=${weatherapi}'
-
-  var weatherbyzip = apiCall + input1.value;
-
-  request = new XMLHttpRequest();
-
-  request.open('GET', weatherbyzip, true);
-  request.onload = function() {
-    var data = JSON.parse(this.response);
-    if (request.status >=200 && request.status < 400) {
-      console.log(data);
-    }
-
-    else {
-      console.log(input1.value);
-    }
-  }
-
-  request.send();
-
-}
-
-
-//Make weather background white   //
-
-// document.getElementById(".weather-feed").style.backgroundColor = rgb(255, 255, 255, .6);
-
-     //Make weather background white   //
-    
-      // document.getElementById(".weather-feed").style.backgroundColor = rgb(255, 255, 255, .6);
-    
