45aa6683b5c3677ee18976e493b840c8dbe88428
LewRit <lewrit16@gmail.com>
2020-09-21 23:02:20 -0400

    fixed weather API

--- weather.js
+++ weather.js
@@ -1,9 +1,15 @@
 $().ready(() =>{
 
+  $('#searchbar').submit((e)=>{
+    e.preventDefault()
+    let someZipCode=document.getElementById('searchbyzip').value;
+    console.log(someZipCode)
+    getWeatherData(someZipCode);
+ 
+  })
 
 
-  getWeatherData();
-
+  
 
 })
 
@@ -12,11 +18,11 @@ $().ready(() =>{
 //$('.form').click()
 
 function getWeatherData(zip) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${someZipCode},us&appid=${weather}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${weather}`)
     .then(data => data.json())
     .then(jsonData => {
 
-    console.log(`\nWEATHER DATA\n${JSON.stringify(jsonData, null, 2)}\n`);
+    // console.log(`\nWEATHER DATA\n${JSON.stringify(jsonData, null, 2)}\n`);
 
     
     let kelvinCurrent = jsonData.main.temp
@@ -28,20 +34,17 @@ function getWeatherData(zip) {
     let kelvinMax = jsonData.main.temp_max
     let fahrenheitMax = ((kelvinMax - 273.15)*1.8)+32
 
-
-
-    $(".weather").append(`<div class ="weather-data">
-    
-    <div class = "weather-name">
+    $(".weather-name").html(`
       <h5>${jsonData.name}</h5>
       <h5>${fahrenheitCurrent.toFixed()} &#176; Fahrenheit</h5>
       <h5>${fahrenheitMin.toFixed()} &#176; Fahrenheit</h5>
       <h5>${fahrenheitMax.toFixed()} &#176; Fahrenheit</h5>
       <h5>${jsonData.weather[0].description} </h5>`)
+    
+      
  
-      }
-
-    )}
+    })
+}
       
       // const someZipCode = "90210";
 
@@ -50,18 +53,4 @@ function getWeatherData(zip) {
       //   let someZipCode=searchbyzip
       //   console.log(someZipCode)
       // }
-      $('#searchbyzip').submit((e)=>{
-        e.preventDefault()
-        let someZipCode=
-        console.log(someZipCode)
-      })
-  
-
-//Make weather background white   //
-
-// document.getElementById(".weather-feed").style.backgroundColor = rgb(255, 255, 255, .6);
-
-     //Make weather background white   //
-    
-      // document.getElementById(".weather-feed").style.backgroundColor = rgb(255, 255, 255, .6);
     
